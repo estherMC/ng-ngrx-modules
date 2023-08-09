1222d5fe5aa8a845f4f7c9c0a684cbe83f2b96eb
Esther Martorell <esther_3@hotmail.com>
2023-08-09 08:53:50 +0200

    :sparkles: attach data to increment

--- src/app/store/counter.actions.ts
+++ src/app/store/counter.actions.ts
@@ -1,6 +1,8 @@
-import { createAction } from "@ngrx/store"
+import { createAction, props } from "@ngrx/store"
 
 export const increment = createAction(
     //Com a standarització, el nom s'indica amb el prefix []
-    '[Counter] Increment'
+    '[Counter] Increment',
+    //value pot ser email and password
+    props<{value: number}>()
 );
