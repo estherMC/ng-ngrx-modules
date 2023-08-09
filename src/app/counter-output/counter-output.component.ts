f5a8392c61bf2c550daee8890c979fa588d928a2
Esther Martorell <esther_3@hotmail.com>
2023-08-09 08:32:32 +0200

    :sparkles: read from the store

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,28 +1,39 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 import { CounterService } from '../counter.service';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css'],
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
-  counter = 0;
-  counterServiceSub?: Subscription;
+export class CounterOutputComponent {
+  
+  //Convenció: en els observables afegir $ final
+  count$: Observable<number>;
 
-  constructor(private counterService: CounterService) {}
-
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe(
-      (newVal) => (this.counter = newVal)
-    );
+  constructor(private store: Store<{counter: number}>){
+    //La key s'extreu del app.module.ts del imports
+    this.count$ = store.select('counter');
   }
+  
+  //Opció 2: fer servir serveis
+  // counter = 0;
+  // counterServiceSub?: Subscription;
 
-  ngOnDestroy(): void {
-    if (this.counterServiceSub) {
-      this.counterServiceSub.unsubscribe();
-    }
-  }
+  // constructor(private counterService: CounterService) {}
+
+  // ngOnInit(): void {
+  //   this.counterServiceSub = this.counterService.counterChanged.subscribe(
+  //     (newVal) => (this.counter = newVal)
+  //   );
+  // }
+
+  // ngOnDestroy(): void {
+  //   if (this.counterServiceSub) {
+  //     this.counterServiceSub.unsubscribe();
+  //   }
+  // }
 }
